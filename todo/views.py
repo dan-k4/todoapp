cfb471ebe5454d3d0ddf68f5d777ba00bdb55ad5
Kawazu Dan <dan@D.local>
2021-07-28 05:41:13 +0900

    first commit

---
+++ todo/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import generic
+from .models import TodoModel
+from django.urls import reverse_lazy
+
+
+class TodoList(generic.ListView):
+    template_name = 'todo/list.html'
+    model = TodoModel
+
+class TodoDetail(generic.DetailView):
+    template_name = 'todo/detail.html'
+    model = TodoModel
+
+class TodoUpdate(generic.UpdateView):
+    template_name = 'todo/update.html'
+    model = TodoModel
+    fields = ('title', 'content', 'deadline')
+    success_url = reverse_lazy('list')
+
+class TodoDelete(generic.DeleteView):
+    template_name = 'todo/delete.html'
+    model = TodoModel
+
+    success_url = reverse_lazy('list')
+
+class TodoCreate(generic.CreateView):
+    template_name = 'todo/create.html'
+    model = TodoModel
+    fields = ('title', 'content', 'deadline')
+
+    success_url = reverse_lazy('list')
